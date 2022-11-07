b8c62836c5775c614a7b2b66eac8cab20a9bb3cc
mirzaistiakhislam <mirza15-9603@diu.edu.bd>
2022-11-07 20:41:19 +0600

    add header

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { FaDev } from "react-icons/fa";
+import LeftSideNav from '../LeftSideNav/LeftSideNav';
 
 const Header = () => {
     return (
         <div>
-            <h2>This is header</h2>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home"><FaDev></FaDev> Web Development Tutorials</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#features">Courses</Nav.Link>
+                            <Nav.Link href="#pricing">FAQ</Nav.Link>
+                            <Nav.Link href="#pricing">Blog</Nav.Link>
+                            <Nav.Link href="#deets">Login</Nav.Link>
+                        </Nav>
+                        <div className='d-lg-none'>
+                            <LeftSideNav></LeftSideNav>
+                        </div>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
