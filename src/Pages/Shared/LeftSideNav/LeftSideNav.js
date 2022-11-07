64e60cd6c3a487c48a0c294485729453463e3b1f
mirzaistiakhislam <mirza15-9603@diu.edu.bd>
2022-11-07 20:01:50 +0600

    update categories

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
 
@@ -13,6 +14,12 @@ const LeftSideNav = () => {
     return (
         <div>
             <h2>All Category: {categories.length}</h2>
+            {
+                categories.map(category =>
+                    <p key={category.id}>
+                        <Link to={`/category/${category.id}`}>{category.name}</Link>
+                    </p>)
+            }
         </div>
     );
 };
