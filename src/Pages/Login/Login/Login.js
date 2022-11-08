f37601cdf8dfd09ca946964e7b3f279ce5c71e53
mirzaistiakhislam <mirza15-9603@diu.edu.bd>
2022-11-08 22:32:55 +0600

    add register & login form

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -7,7 +7,7 @@ import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const Login = () => {
 
-    const { googleProviderLogin } = useContext(AuthContext);
+    const { googleProviderLogin, signIn } = useContext(AuthContext);
     const googleProvider = new GoogleAuthProvider()
 
     const handleGoogleSignIn = () => {
@@ -18,17 +18,31 @@ const Login = () => {
             .catch(error => console.error(error))
     }
 
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                form.reset();
+            })
+            .catch(error => console.error(error));
+    }
+
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control type="email" placeholder="Enter Email" />
+                <Form.Label>Your Email Address</Form.Label>
+                <Form.Control type="email" name="email" placeholder="Enter Your Email" required />
 
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Enter Password" />
+                <Form.Label>Your Password</Form.Label>
+                <Form.Control type="password" name="password" placeholder="Enter Your Password" required />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Button variant="primary" type="submit">
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const Register = () => {
-    return (
-        <div>
 
-        </div>
+    const { createUser } = useContext(AuthContext);
+
+    const handleSubmit = event => {
+
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const photoURL = form.photoURLvalue;
+        const password = form.password.value;
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                form.reset();
+            })
+            .catch(error => console.error(error));
+
+    }
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Your Full Name</Form.Label>
+                <Form.Control type="text" name="name" placeholder="Enter Full Name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Your Email Address</Form.Label>
+                <Form.Control type="email" name="email" placeholder="Enter Your Email" required />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Your PhotoURL</Form.Label>
+                <Form.Control type="text" name="photoURL" placeholder="Enter PhotoURL" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Your Password</Form.Label>
+                <Form.Control type="password" name="password" placeholder="Enter Your Password" required />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Button variant="primary" type="submit">
+                    Register
+                </Button>
+            </Form.Group>
+            <Form.Text className="text-danger">
+                We'll never share your email with anyone else.
+            </Form.Text> <br />
+
+        </Form>
     );
 };
 
